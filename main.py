725d30ef5f9fc6c5925862a30a51abf70710c95e
Saviel <savman1994@gmail.com>
2021-10-09 16:21:49 +0200

    Code now gets all reviews, ratings and usernames and puts them into a single array

--- main.py
+++ main.py
@@ -15,56 +15,14 @@ def getData(pageNumber):
     # print(soup.get_text())
     # print("getting text")
 
-    reviews = []
-    ratings = []
-    names = []
+    allReviews = []
 
     for unorederedList in soup.findAll('ul', attrs={'class':'undefined list__373c0__vNxqp'}):
         for block in unorederedList.findAll('div', attrs={'class': 'review__373c0__3MsBX border-color--default__373c0__1WKlL'}):
-            reviews.append(block.find('p', attrs={'class':'comment__373c0__Nsutg'}))
-            ratings.append(block.find('div', attrs={'class':'i-stars__373c0___sZu0'}))
-            names.append(block.find('span', attrs={'class':'fs-block css-m6anxm'}))
-
-    print("LENGHTS")
-    print(len(names))
-    print(len(ratings))
-    print(len(reviews))
-
-    # for i in range(0, 10):
-    #     realName = names[i].find('a')
-    #     if realName is not None:
-    #         print(realName.text)
-
-    #     print(reviews[i])
-    #     print(ratings[i].text)
-
-
-    print ("//////////////////////////////////////")
-    print ("NAMES")
-    # for i in range(1, 11):
-    #     realName = names[i].find('a')
-    #     if realName is not None:
-    #         print(realName.text)
-
-        # print(reviews[i-1])
-
-    # for name in names:
-    #     realName = name.find('a')
-
-    #     if realName is not None:
-    #         print(realName.text)
-    
-    print ("//////////////////////////////////////")
-    print ("REVIEWS")
-    # for review in reviews:
-    #     if review is not None:
-    #         print(review.text)
-
-    print ("//////////////////////////////////////")
-    print ("RATINGS")
-    # for rating in ratings:
-    #     print(ratings)
-    #     if rating is not None:
-    #         print(rating.text)
+            reviewBlock = []
+            reviewBlock.append(block.find('p', attrs={'class':'comment__373c0__Nsutg'}).text)
+            reviewBlock.append(block.find('div', attrs={'class':'i-stars__373c0___sZu0'})['aria-label'])
+            reviewBlock.append(block.find('span', attrs={'class':'fs-block css-m6anxm'}).text)
+            allReviews.append(reviewBlock)
 # for i in range(0, numberOfPages):
 getData(0)
