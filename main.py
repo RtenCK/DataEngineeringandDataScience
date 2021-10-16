9d0b0402308aa6efe23a3da70c349f6e178e7466
Saviel <savman1994@gmail.com>
2021-10-16 17:26:25 +0200

    working on creating and clean a corpus

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+import nltk
+import re
+from nltk.corpus import stopwords                   #Stopwords corpus
+from nltk.stem import PorterStemmer                 # Stemmer
+
+from sklearn.feature_extraction.text import CountVectorizer          #For Bag of words
+from sklearn.feature_extraction.text import TfidfVectorizer          #For TF-IDF
+
 numberOfPages = 4
 allReviews = []
 
@@ -41,8 +49,6 @@ def partition(x):
 # reviewDataFrame = pd.DataFrame(data = allReviews, columns=['Review', 'Rating', 'Author'])
 # reviewDataFrame.to_csv('yelp_reviews.csv')
 
-
-
 # READ CSV's
 reviewDataFrame = pd.read_csv('yelp_reviews.csv')
 print(reviewDataFrame.columns)
@@ -50,5 +56,26 @@ print(reviewDataFrame.columns)
 labelColumn = reviewDataFrame['Rating'].map(partition)
 reviewDataFrame['labelColumn'] = labelColumn
 print(reviewDataFrame)
+
+final_X = reviewDataFrame['Review']
+final_y = reviewDataFrame['Rating']
+
+nltk.download('stopwords')
+stop = set(stopwords.words('english'))
+
+temp =[]
+
+snow = nltk.stem.SnowballStemmer('english')
+for sentence in final_X:
+    sentence = sentence.lower()                 # Converting to lowercase
+    cleanr = re.compile('<.*?>')
+    sentence = re.sub(cleanr, ' ', sentence)        #Removing HTML tags
+    sentence = re.sub(r'[?|!|\'|"|#]',r'',sentence)
+    sentence = re.sub(r'[.|,|)|(|\|/]',r' ',sentence)        #Removing Punctuations
+    
+    words = [snow.stem(word) for word in sentence.split() if word not in stopwords.words('english')]   # Stemming and removing stopwords
+    temp.append(words)
+    
+final_X = temp 
 
 # labelColumn = reviewDataFrame.map(reviewDataFrame['Review'])
