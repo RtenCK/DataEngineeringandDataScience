c480be436b1b17c9cc5c90a54427ad61a2105a32
Saviel <savman1994@gmail.com>
2021-10-16 15:07:20 +0200

    fixed dataframe

--- main.py
+++ main.py
@@ -29,10 +29,9 @@ def getData(pageNumber):
             # print(reviewBlock)
             reviewBlock.append(block.find('span', attrs={'class':'fs-block css-m6anxm'}).text)
             allReviews.append(reviewBlock)
-            print(allReviews[0])
+            print(allReviews)
     
     
-    flatten = lambda l: [item for sublist in l for item in sublist]
-    reviewDataFrame = pd.DataFrame(flatten(allReviews), columns=['Review', 'Rating', 'Author'])
+    reviewDataFrame = pd.DataFrame(data = allReviews, columns=['Review', 'Rating', 'Author'])
 # for i in range(0, numberOfPages):
 getData(0)
